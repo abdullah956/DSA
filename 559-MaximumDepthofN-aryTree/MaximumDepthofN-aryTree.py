fdda7ec6410b6ed59bba3011391d37e29221a4c2
Abdullah <abdullaharshed956@gmail.com>
2024-03-13 07:19:50 +0500

    revised

--- 559-MaximumDepthofN-aryTree/MaximumDepthofN-aryTree.py
+++ 559-MaximumDepthofN-aryTree/MaximumDepthofN-aryTree.py
@@ -17,6 +17,17 @@ class Solution:
             for child in node.children:
                 stack.append((child, d+1))
         return depth
+
+        stack=[]
+        if root :
+            stack.append((root,1))
+        depth=0
+        while stack :
+            (node,d) = stack.pop()
+            depth=max(depth,d)
+            for child in node.children :
+                stack.append((child,d+1))
+        return depth
 class Solution:
     def maxDepth(self, root):
         queue = []
