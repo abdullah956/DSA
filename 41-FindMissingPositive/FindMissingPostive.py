d31e7deb906d181e680645c934b45c11f2df393f
Abdullah <abdullaharshed956@gmail.com>
2024-04-23 11:10:16 +0500

    new

---
+++ 41-FindMissingPositive/FindMissingPostive.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # Function to swap elements in the array
+        def swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+        n = len(nums)
+        print("orignally",nums)
+        # Place each positive integer i at index i-1 if possible
+        for i in range(n):
+            while 0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                swap(nums, i, nums[i] - 1)
+        print("secondly",nums)
+        # Find the first missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        # If all positive integers from 1 to n are present, return n + 1
+        return n + 1
+s = Solution()
+print(s.firstMissingPositive([7,8,9,11,12]))
