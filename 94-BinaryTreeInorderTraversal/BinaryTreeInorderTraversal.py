bbef882aacdce1e5e1be5f6cd849c986c7e869e5
abdullah956 <abdullaharshed956@gmail.com>
2024-07-08 11:53:20 +0500

    94

--- 94-BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.py
+++ 94-BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.py
@@ -1,20 +1,30 @@
+from typing import List
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from typing import List
+
 class Solution:
     def inorderTraversal(self, root) -> List[int]:
-        result = []
-        stack = [] 
-        current_node = root 
-        while current_node or stack :
-            while current_node :
-                stack.append(current_node)
-                current_node = current_node.left
-            current_node = stack.pop()
-            result.append(current_node.val)
+        result = []  # This will store the values of nodes in inorder sequence
+        stack = []  # Stack to keep track of nodes
+        current_node = root  # Start with the root node
+        
+        # Iterate while there are nodes to be processed
+        while current_node or stack:
+            # Reach the leftmost node of the current_node
+            while current_node:
+                stack.append(current_node)  # Push the current node to the stack
+                current_node = current_node.left  # Move to the left child
+            
+            # current_node is now None, process the node on top of the stack
+            current_node = stack.pop()  # Pop the top node from the stack
+            result.append(current_node.val)  # Append the node's value to the result
+            
+            # We have visited the node and its left subtree, now it's right subtree's turn
             current_node = current_node.right
-        return result
+        
+        return result  # Return the final inorder traversal result
